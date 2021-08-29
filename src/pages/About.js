70fa852ff4738f86f5169b074bc5af1634285903
aniket193-web <80771166+aniket193-web@users.noreply.github.com>
2021-08-29 15:24:13 +0530

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import AboutUs from '../AboutUS'
+import Footer from '../Footer'
+const About = () => {
+    return (<>
+    <Navbar />
+    <AboutUs />
+    <Footer />
+    </>
+
+    )
+}
+
+export default About
